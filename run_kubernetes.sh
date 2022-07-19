3e4a0f3183362992697235df348ffd883dddedd7
Peter Adeoye <cwizard2011@gmail.com>
2022-07-19 08:23:48 +0000

    add log for kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,11 +8,11 @@ dockerpath=cwizard/devop-ml-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy python-app --image=cwizard/devop-ml-prediction
+kubectl create deploy devop-ml-prediction  --image=cwizard/devop-ml-prediction
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/*** --address 0.0.0.0 8000:80
+kubectl port-forward pod/devop-ml-prediction-9659fd557-g4jtc --address 0.0.0.0 8000:80
